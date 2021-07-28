eea2f226b9c0bb69254b168d13f686201526f798
Thiago F. Ribeiro <thiagofribeiro@gmail.com>
2021-07-28 16:43:08 -0300

    Adding style for movie item

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,12 +1,13 @@
 import React, { ReactElement } from 'react';
 import { romanize } from 'romans';
 import IMovie from '../types/IMovie';
+import styles from './MovieItem.module.css';
 
 function MovieList({ movie }: {movie: IMovie}): ReactElement {
   return (
-    <p data-testid="movie-list-item">
+    <div className={styles.movieItem} data-testid="movie-list-item">
       {`Episode ${romanize(movie.episode_id)} - ${movie.title} (${movie.release_date.split('-')[0]})`}
-    </p>
+    </div>
   );
 }
 
