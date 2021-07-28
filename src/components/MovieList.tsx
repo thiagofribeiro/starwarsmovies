11134cca33f9f665ca774c0b810b521249158953
Thiago F. Ribeiro <thiagofribeiro@gmail.com>
2021-07-28 16:21:38 -0300

    fix: data-testid attribute

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -7,10 +7,10 @@ function MovieList(): ReactElement {
 
   if (isLoading) return <div data-testid="movie-list">Loading...</div>;
 
-  if (error) return <div data-container="movie-list">{`An error has occurred: ${(error as Error).message}`}</div>;
+  if (error) return <div data-testid="movie-list">{`An error has occurred: ${(error as Error).message}`}</div>;
 
   return (
-    <div data-container="movie-list">
+    <div data-testid="movie-list">
       { data && data.count > 0
         && data.results.map((item: IMovie) => (
           <p key={item.episode_id}>
