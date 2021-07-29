adc243d4c8cdcd1b72322216c067abc1ab170cf9
Thiago F. Ribeiro <thiagofribeiro@gmail.com>
2021-07-29 02:24:37 -0300

    Add transitions effects

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,17 +1,31 @@
-import React, { ReactElement, ReactNode } from 'react';
+import React, {
+  LegacyRef, ReactElement, ReactNode, useRef,
+} from 'react';
 import styles from './Overlay.module.css';
 
 function Overlay(
   { children, onClose } : {children: ReactNode, onClose: () => void },
 ): ReactElement {
+  const overlayEl:LegacyRef<HTMLDivElement> = useRef(null);
+
+  const handleClick = () => {
+    overlayEl.current?.classList.add(styles.fadeOut);
+    const idTimeout = setTimeout(() => {
+      clearTimeout(idTimeout);
+      onClose();
+    }, 1000);
+  };
+
   return children
     ? (
       <div className={styles.overlay}>
-        <button type="button" className={styles.close} onClick={onClose}>
-          <span className={styles.closeSign}>&times;</span>
-          <span className={styles.closeText}>close</span>
-        </button>
-        { children }
+        <div ref={overlayEl} className={styles.overlayContainer}>
+          <button type="button" className={styles.close} onClick={handleClick}>
+            <span className={styles.closeSign}>&times;</span>
+            <span className={styles.closeText}>close</span>
+          </button>
+          { children }
+        </div>
       </div>
     )
     : <></>;
