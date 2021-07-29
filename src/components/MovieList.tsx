aeae71976b07defa9f1691b836d81569c1d2c8be
Thiago F. Ribeiro <thiagofribeiro@gmail.com>
2021-07-29 00:03:11 -0300

    Extract getMovies Function

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -5,25 +5,39 @@ import MovieItem from './MovieItem';
 import Overlay from './Overlay';
 import IResponse from '../types/IResponse';
 
+const getMovies = async () => {
+  const response = await fetch('https://swapi.dev/api/films/');
+
+  if (!response.ok) {
+    throw new Error('Problem fetching user');
+  }
+
+  const responseData = await response.json();
+  if (!(responseData && responseData.count > 0)) {
+    throw new Error('No movies founded');
+  }
+
+  return responseData;
+};
+
 function MovieList(): ReactElement {
-  const { isLoading, error, data } = useQuery<boolean, Error, IResponse<IMovie>>('repoData', () => fetch('https://swapi.dev/api/films/').then((res) => res.json()));
+  const { isLoading, error, data } = useQuery<boolean, Error, IResponse<IMovie>>('repoData', getMovies);
   const [selectedMovie, setSelectedMovie] = useState<IMovie | null>();
 
   if (isLoading) return <div data-testid="movie-list">Loading...</div>;
 
-  if (error) return <div data-testid="movie-list">{`An error has occurred: ${error.message}`}</div>;
+  if (error) return <div data-testid="movie-list">{`${error.message}`}</div>;
 
   return (
     <>
       <div data-testid="movie-list">
-        { data && data.count > 0
-          && data.results.map((item) => (
-            <MovieItem
-              key={item.episode_id}
-              movie={item}
-              onSelect={(movie) => setSelectedMovie(movie)}
-            />
-          )) }
+        { data && data.results.map((item) => (
+          <MovieItem
+            key={item.episode_id}
+            movie={item}
+            onSelect={(movie) => setSelectedMovie(movie)}
+          />
+        )) }
       </div>
       <Overlay
         onClose={() => setSelectedMovie(null)}
