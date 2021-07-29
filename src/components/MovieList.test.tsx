a7209c34a93f04a69962a709a9c5bf8a55d36b29
Thiago F. Ribeiro <thiagofribeiro@gmail.com>
2021-07-29 00:39:58 -0300

    Fix on test

--- src/components/MovieList.test.tsx
+++ src/components/MovieList.test.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import MovieList from './MovieList';
+import { MoviesContextProvider } from '../contexts/movies';
 
 const queryClient = new QueryClient();
 
 describe('movie list tests', () => {
   test('renders movie list element', () => {
-    render(<QueryClientProvider client={queryClient}><MovieList /></QueryClientProvider>);
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MoviesContextProvider>
+          <MovieList />
+        </MoviesContextProvider>
+      </QueryClientProvider>,
+    );
     const movieListElement = screen.getByTestId('movie-list');
     expect(movieListElement).toBeInTheDocument();
   });
